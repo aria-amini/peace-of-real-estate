6720410d4ca052361853a10a285db6b33377ff6f
Aria Amini <amini5454@gmail.com>
2026-05-27 15:39:40 -0400

    fix config

--- config/test/server.ts
+++ config/test/server.ts
@@ -1,18 +1,1 @@
-import { test as baseTest } from 'vite-plus/test'
-
-import { startMswServer } from './msw-server'
-
-const extended = baseTest
-	.extend(
-		'server',
-		{ auto: true, scope: 'worker' },
-		async ({}, { onCleanup }) => {
-			return startMswServer(onCleanup)
-		},
-	)
-	.extend('_cleanup', { auto: true }, ({ server }, { onCleanup }) => {
-		onCleanup(() => server.resetHandlers())
-	})
-
-export { afterEach, beforeEach, describe, expect, vi } from 'vite-plus/test'
-export const test = extended
+export * from '@aamini/config/test/server'
