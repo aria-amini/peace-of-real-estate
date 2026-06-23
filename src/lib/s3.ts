2e087693fb12e51e890776141ca73d8a3227ff5b
Aria Amini <amini5454@gmail.com>
2026-06-23 07:38:47 -0400

    minior bug

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -3,11 +3,11 @@ import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 import { serverEnv as env } from '@/env.server'
 
 const storageClient = new S3Client({
-	region: env.AWS_REGION ?? 'auto',
-	endpoint: env.AWS_ENDPOINT_URL ?? '',
+	region: env.AWS_REGION,
+	endpoint: env.AWS_ENDPOINT_URL,
 	credentials: {
-		accessKeyId: env.AWS_ACCESS_KEY_ID ?? '',
-		secretAccessKey: env.AWS_SECRET_ACCESS_KEY ?? '',
+		accessKeyId: env.AWS_ACCESS_KEY_ID,
+		secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
 	},
 	forcePathStyle: true,
 })
